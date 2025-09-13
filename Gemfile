0e37d88fad3eb1d5daacf5b8927a4575dea33925
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-09-12 23:08:14 -0400

    finished the codio and tried messing with guard autotester gem

--- Gemfile
+++ Gemfile
@@ -2,7 +2,13 @@ source 'http://rubygems.org'
 
 ruby '~> 3.3', '>= 3.3.8'
 
-gem 'rspec',    '~> 3.13'
 gem 'cucumber', '~> 9.2'
+gem 'rspec',    '~> 3.13'
 
 gem 'byebug', '~> 12.0'
+
+group :development do
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'pry'
+end
--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -48,11 +48,24 @@ def binary_multiple_of_4?(s)
   return false unless s.match?(/\A[10]+\z/)
 
   num = s.to_i(2)
-  num % 4 == 0
+  (num % 4).zero?
 end
 
 # Part 3
 
 class BookInStock
   # YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty?
+    raise ArgumentError if price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$#{format('%.2f', price)}"
+  end
 end
